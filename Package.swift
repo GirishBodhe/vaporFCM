3cea7cace81ce15e058d85e980ebc5f4d1fbdce1
GirishBodhe <bodhegirish@gmail.com>
2018-08-17 16:23:40 +0530

    AUTH test

--- Package.swift
+++ Package.swift
@@ -10,8 +10,9 @@ let package = Package(
 //        // 🔵 Swift ORM (queries, models, relations, etc) built on SQLite 3.
         .package(url: "https://github.com/vapor/fluent-sqlite.git", from: "3.0.0-rc.2"),
 
-        .package(url: "https://github.com/MihaelIsaev/FCM.git", from: "0.4.1"),
-//
+        .package(url: "https://github.com/MihaelIsaev/FCM.git", from: "0.5.0"),
+
+        
         // 👤 Authentication and Authorization layer for Fluent.
         .package(url: "https://github.com/vapor/auth.git", from: "2.0.0"),
         .package(url: "https://github.com/vapor/crypto.git", from : "3.0.0")
